ac46b3316dbc52f22369e27e26e0ac9270da5c38
Viktoria Marchenko <92745008+vsmarrch@users.noreply.github.com>
2023-04-09 04:06:36 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,9 +4,9 @@ import numpy as np
 
 chat_id = 260376781 # Ваш chat ID, не меняйте название переменной
 
-from scipy.stats import ks_2samp
+from scipy.stats import cramervonmises_2samp
 
 def solution(x: np.array, y: np.array) -> bool:
-    p_val = ks_2samp(x, y).pvalue
+    p_val = cramervonmises_2samp(x, y).pvalue
     answer = False if p_val > 0.01 else True
     return answer # Ваш ответ, True или False
