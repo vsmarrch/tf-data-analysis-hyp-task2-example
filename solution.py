762da880840071ee37b85644a0063b97162dc37e
Viktoria Marchenko <92745008+vsmarrch@users.noreply.github.com>
2023-04-12 23:59:44 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,9 +4,9 @@ import numpy as np
 
 chat_id = 260376781 # Ваш chat ID, не меняйте название переменной
 
-from scipy.stats import anderson_ksamp
+from hyppo.ksample import MMD
 
 def solution(x: np.array, y: np.array) -> bool:
-    p_val = anderson_ksamp([x, y]).significance_level
+    stat, p_val = MMD(compute_kernel="laplacian").test(x, y)
     answer = False if p_val > 0.01 else True
     return answer # Ваш ответ, True или False
